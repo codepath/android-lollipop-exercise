ca972ced15956fb4256ac2e8b95a6fdeabe61174
koalahamlet <koalahamlet@gmail.com>
2015-06-27 11:59:03 -0700

    add solution work

--- app/src/main/java/com/codepath/android/lollipopexercise/adapters/ContactsAdapter.java
+++ app/src/main/java/com/codepath/android/lollipopexercise/adapters/ContactsAdapter.java
@@ -2,6 +2,14 @@ package com.codepath.android.lollipopexercise.adapters;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,8 +18,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.codepath.android.lollipopexercise.R;
+import com.codepath.android.lollipopexercise.activities.ContactsActivity;
+import com.codepath.android.lollipopexercise.activities.DetailsActivity;
 import com.codepath.android.lollipopexercise.models.Contact;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import java.util.List;
 
@@ -37,11 +48,47 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.VH> {
 
     // Display data at the specified position
     @Override
-    public void onBindViewHolder(VH holder, int position) {
+    public void onBindViewHolder(final VH holder, int position) {
+
         Contact contact = mContacts.get(position);
         holder.rootView.setTag(contact);
         holder.tvName.setText(contact.getName());
         Picasso.with(mContext).load(contact.getThumbnailDrawable()).into(holder.ivProfile);
+        // Define a listener for image loading
+        Target target = new Target() {
+            // Fires when Picasso finishes loading the bitmap for the target
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                // TODO 1. Insert the bitmap into the profile image view
+                holder.ivProfile.setImageBitmap(bitmap);;
+                // TODO 2. Use generateAsync() method from the Palette API to get the vibrant color from the bitmap
+                //      Set the result as the background color for `R.id.vPalette` view containing the contact's name.
+                Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+                    @Override
+                    public void onGenerated(Palette palette) {
+                        int color = palette.getVibrantColor(Color.WHITE);
+                        holder.vPalette.setBackgroundColor(color);
+                        holder.vPalette.setTag(color);
+                    }
+                });
+            }
+
+            // Fires if bitmap fails to load
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable) {
+
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+            }
+        };
+
+        // Store the target into the tag for the profile to ensure target isn't garbage collected prematurely
+            holder.ivProfile.setTag(target);
+// Instruct Picasso to load the bitmap into the target defined above
+            Picasso.with(mContext).load(contact.getThumbnailDrawable()).into(target);
     }
 
     @Override
@@ -63,6 +110,7 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.VH> {
             tvName = (TextView)itemView.findViewById(R.id.tvName);
             vPalette = itemView.findViewById(R.id.vPalette);
 
+
             // Navigate to contact details activity on click of card view.
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -71,6 +119,22 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.VH> {
                     if (contact != null) {
                         // Fire an intent when a contact is selected
                         // Pass contact object in the bundle and populate details activity.
+                        Intent i = new Intent(context, DetailsActivity.class);
+                        Bundle b = new Bundle();
+                        b.putSerializable(DetailsActivity.EXTRA_CONTACT, contact);
+
+                        b.putInt("color", (int) vPalette.getTag());
+                        i.putExtras(b);
+
+                        // Create transition
+                        ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                                ((ContactsActivity) context),
+                                new Pair<View, String>(ivProfile, "profile"),
+                                new Pair<View, String>(vPalette, "palette"),
+                                new Pair<View, String>(tvName, "name")
+                        );
+
+                        context.startActivity(i, options.toBundle());
                     }
                 }
             });
