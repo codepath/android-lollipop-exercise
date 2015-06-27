206ada54bd3172b93557260ff6d8c657903645f8
koalahamlet <koalahamlet@gmail.com>
2015-06-27 11:59:03 -0700

    add solution work

--- app/src/main/java/com/codepath/android/lollipopexercise/activities/DetailsActivity.java
+++ app/src/main/java/com/codepath/android/lollipopexercise/activities/DetailsActivity.java
@@ -1,9 +1,16 @@
 package com.codepath.android.lollipopexercise.activities;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.transition.Transition;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,22 +19,28 @@ import com.codepath.android.lollipopexercise.models.Contact;
 import com.squareup.picasso.Picasso;
 
 public class DetailsActivity extends AppCompatActivity {
+	public static final String TAG = DetailsActivity.class.getSimpleName();
     public static final String EXTRA_CONTACT = "EXTRA_CONTACT";
     private Contact mContact;
     private ImageView ivProfile;
     private TextView tvName;
     private TextView tvPhone;
     private View vPalette;
+    private FloatingActionButton fab;
+    private Transition.TransitionListener mEnterTransitionListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-
         ivProfile = (ImageView) findViewById(R.id.ivProfile);
         tvName = (TextView) findViewById(R.id.tvName);
         tvPhone = (TextView) findViewById(R.id.tvPhone);
         vPalette = findViewById(R.id.vPalette);
+        vPalette.setBackgroundColor(getIntent().getExtras().getInt("color"));
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // Extract contact from bundle
         mContact = (Contact)getIntent().getExtras().getSerializable(EXTRA_CONTACT);
@@ -36,11 +49,129 @@ public class DetailsActivity extends AppCompatActivity {
         Picasso.with(DetailsActivity.this).load(mContact.getThumbnailDrawable()).into(ivProfile);
         tvName.setText(mContact.getName());
         tvPhone.setText(mContact.getNumber());
+        // enter transition listener for making a circular view work
+        mEnterTransitionListener = new Transition.TransitionListener() {
+            @Override
+            public void onTransitionStart(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                // once the page is done, start the circular reveal.
+                enterReveal();
+            }
+
+            @Override
+            public void onTransitionCancel(Transition transition) {
+
+			}
+
+            @Override
+            public void onTransitionPause(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionResume(Transition transition) {
+
+            }
+        };
+        getWindow().getEnterTransition().addListener(mEnterTransitionListener);
+
+        // Dial contact's number.
+        // This shows the dialer with the number, allowing you to explicitly initiate the call.
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uri = "tel:" + mContact.getNumber();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(uri));
+                startActivity(intent);
+            }
+        });
+
+    }
+
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+				// Trigger the exit animation and process
+                exitReveal();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    void enterReveal() {
+        // get the center for the clipping circle
+        int cx = fab.getMeasuredWidth() / 2;
+        int cy = fab.getMeasuredHeight() / 2;
+
+        // get the final radius for the clipping circle
+        int finalRadius = Math.max(fab.getWidth(), fab.getHeight()) / 2;
+
+        // create the animator for this view (the start radius is zero)
+        Animator anim =
+                ViewAnimationUtils.createCircularReveal(fab, cx, cy, 0, finalRadius);
+
+        //Start the animation and make the view visible
+		fab.setVisibility(View.VISIBLE);
+        anim.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+				// cleanup our transition listener
+                getWindow().getEnterTransition().removeListener(mEnterTransitionListener);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        anim.start();
+    }
+
+    void exitReveal() {
+        // get the center for the clipping circle
+        int cx = fab.getMeasuredWidth() / 2;
+        int cy = fab.getMeasuredHeight() / 2;
+        // get the initial radius for the clipping circle
+        int initialRadius = fab.getWidth() / 2;
+
+        // create the animation (the final radius is zero)
+        Animator anim =
+                ViewAnimationUtils.createCircularReveal(fab, cx, cy, initialRadius, 0);
+
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                // Finish the activity after the exit transition completes and make it invisible
+				findViewById(R.id.fab).setVisibility(View.INVISIBLE);
+                supportFinishAfterTransition();
+            }
+        });
+        // start the animation
+        anim.start();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+	// This intercepts when the phones back button is pressed and triggers our exit animation
+    public void onBackPressed() {
+        exitReveal();
     }
 
 }
