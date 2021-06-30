5897274915c61dcd66908bae68478fc985bc6628
Tejen Patel <me@tejen.net>
2021-06-30 00:06:41 -0700

    2021 Temp Edit

--- app/src/main/java/com/codepath/android/lollipopexercise/models/Contact.java
+++ app/src/main/java/com/codepath/android/lollipopexercise/models/Contact.java
@@ -37,16 +37,20 @@ public class Contact implements Serializable {
     // Returns a list of contacts
     public static List<Contact> getContacts() {
         List<Contact> contacts = new ArrayList<>();
-        contacts.add(new Contact("Eric", R.drawable.contact_nine, "4153508889"));
-        contacts.add(new Contact("Victor", R.drawable.contact_four, "4153508884"));
-        contacts.add(new Contact("Kara", R.drawable.contact_two, "4153508882"));
-        contacts.add(new Contact("Anthony", R.drawable.contact_eight, "4153508888"));
-        contacts.add(new Contact("Ernest", R.drawable.contact_ten, "4153508890"));
-        contacts.add(new Contact("TJ", R.drawable.contact_three, "4153508883"));
-        contacts.add(new Contact("Lov", R.drawable.contact_five, "4153508885"));
-        contacts.add(new Contact("Jadal", R.drawable.contact_seven, "4153508887"));
-        contacts.add(new Contact("Yonathan", R.drawable.contact_one, "4153508881"));
-        contacts.add(new Contact("Rando", R.drawable.contact_six, "4153508886"));
+        contacts.add(new Contact("Juan", R.drawable.contact_fourteen, "4153525528"));
+        contacts.add(new Contact("Caston", R.drawable.contact_thirteen, "4153139936"));
+        contacts.add(new Contact("Eric", R.drawable.contact_eight, "4153508889"));
+        contacts.add(new Contact("Rey", R.drawable.contact_one, "4153508890"));
+        contacts.add(new Contact("Aldo", R.drawable.contact_twelve, "4153508885"));
+        contacts.add(new Contact("Anthony", R.drawable.contact_seven, "4153508883"));
+        contacts.add(new Contact("Nafis", R.drawable.contact_four, "4153508882"));
+        contacts.add(new Contact("Josephine", R.drawable.contact_five, "4153508884"));
+        contacts.add(new Contact("Tejen", R.drawable.contact_three, "4153508881"));
+        contacts.add(new Contact("Mark", R.drawable.contact_six, "4153508226"));
+        contacts.add(new Contact("Ellen", R.drawable.contact_two, "4153528880"));
+        contacts.add(new Contact("Lance", R.drawable.contact_eleven, "4153508887"));
+        contacts.add(new Contact("Daniel", R.drawable.contact_nine, "4153508888"));
+        contacts.add(new Contact("Matthew", R.drawable.contact_ten, "4153508886"));
         return contacts;
     }
 
